3dee20d2a7c5389b14a1ede7b327c3a2198178d2
YouRa <allakhverdovyuriy@gmail.com>
2021-04-15 10:10:50 +0300

    Login

--- app.py
+++ app.py
@@ -53,6 +53,28 @@ def register():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+   if request.method == 'POST':
+      user_details = request.form
+      username = user_details['username']
+      cursor = mysql.connection.cursor()
+      result_value = cursor.execute("SELECT * FROM user WHERE username = %s", ([username]))
+      if result_value > 0:
+         user = cursor.fetchone()
+         if check_password_hash(user['password'], user_details['password']):
+            session['login'] = True
+            session['first_name'] = user['first_name']
+            session['last_name'] = user['last_name']
+            flash('Welcome ' + session['first_name'] + '! You have been successfully logged in!', 'success')
+         else:
+            cursor.close
+            flash('Password is incorrect!', 'danger')
+            return render_template('login.html')
+      else:
+         cursor.close()
+         flash('User does not exist!', 'danger')
+         return render_template('login.html')
+      cursor.close()
+      return redirect('/')
    return render_template('login.html')
 
 
